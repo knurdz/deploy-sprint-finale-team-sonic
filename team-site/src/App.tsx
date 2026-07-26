943ef79a76fa5e938e62a4a9c0efaf760a813121
Thush-X <thushanthebest@gmail.com>
2026-07-26 16:14:25 +0530

    T16: add Resend email alert status

--- team-site/src/App.tsx
+++ team-site/src/App.tsx
@@ -10,6 +10,7 @@ import {
   ShieldCheck,
   Users,
 } from 'lucide-react';
+import EmailStatus from "./components/EmailStatus";
 import { ContactForm } from './components/ContactForm';
 import { CourseCard } from './components/CourseCard';
 import { DeadlineBoard } from './components/DeadlineBoard';
@@ -139,6 +140,7 @@ export function App() {
         </section>
 
         <ContactForm />
+        <EmailStatus />
 
       </section>
     </main>
